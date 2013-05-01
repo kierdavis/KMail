1ef6f50893ea5e9ca7d91279529c6fc40f4b20ba
Kier Davis <kierdavis@gmail.con>
2013-05-01 19:20:28 +0100

    Fixed syntax errors

--- src/WebServer.java
+++ src/WebServer.java
@@ -1,19 +1,33 @@
 package com.kierdavis.kmail;
 
 import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.InetSocketAddress;
+import java.net.InetSocketAddress;
 
 public class WebServer {
     private KMail plugin;
+    private HttpServer server;
     
     public WebServer(KMail plugin) {
         this.plugin = plugin;
     }
     
     public void start() {
-        HttpServer server = HttpServer.create(new InetSocketAddress(4880));
+        if (server != null) {
+            stop();
+        }
+        
+        server = HttpServer.create(new InetSocketAddress(4880));
         server.createContext("/", new WebHandler(plugin));
         server.setExecutor(null);
         server.start();
+    }
+    
+    public void stop() {
+        if (server == null) {
+            return;
+        }
+        
+        server.stop(5);
+        server = null;
     }
 }
