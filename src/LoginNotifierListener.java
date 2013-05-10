7e7747eac15609fa69223bf723aa34f6854928dd
Kier Davis <kierdavis@gmail.con>
2013-05-10 20:46:39 +0100

    Made login notifications asynchronous (so they appear nearer the end of the login message block, and to improve performance)

--- src/LoginNotifierListener.java
+++ src/LoginNotifierListener.java
@@ -1,7 +1,5 @@
 package com.kierdavis.kmail;
 
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -16,14 +14,6 @@ public class LoginNotifierListener implements Listener {
     
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        Mailbox mb = plugin.getMailbox(player.getName());
-        int numUnread = mb.numUnread();
-        
-        if (numUnread > 0) {
-            String pluralization = numUnread == 1 ? "" : "s";
-            player.sendMessage(ChatColor.YELLOW + "You have " + ChatColor.GREEN + Integer.toString(numUnread) + ChatColor.YELLOW + " unread message" + pluralization + ".");
-            player.sendMessage(ChatColor.YELLOW + "Type " + ChatColor.DARK_RED + "/kmail read next" + ChatColor.YELLOW + " to begin reading them.");
-        }
+        plugin.runTaskAsynchronously(new DelayedJoinHandler(plugin, event.getPlayer()));
     }
 }
