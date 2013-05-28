1670401e7942fbe8a1fed2f50c5474ad527841d3
Kier Davis <kierdavis@gmail.con>
2013-05-28 18:00:35 +0100

    Fixed compile errors

--- src/KMail.java
+++ src/KMail.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mcstats.Metrics;
